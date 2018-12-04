edc166b34cb32850279fee422fdbacd3f7f54989
Bo Yu <wen.yu@uoit.net>
2018-12-03 19:33:56 -0500

    Extra shtuffff

--- AnimationsAssignment3/Assets/ControllerSupport.cs
+++ AnimationsAssignment3/Assets/ControllerSupport.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class ControllerSupport : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private Animator control;
+    float x;
+    float y;
+    float jump;
+    float strong;
+    float weak;
+
+    // Use this for initialization
+    void Start () {
+        control = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        x = Input.GetAxis("Horizontal");
+        control.SetFloat("x", x);
+        y = Input.GetAxis("Vertical");
+        control.SetFloat("y", y);
+        jump = Input.GetAxis("Jump");
+        control.SetFloat("jump", jump);
+        strong = Input.GetAxis("HeavyAttack");
+        control.SetFloat("strong", strong);
+        weak = Input.GetAxis("WeakAttack");
+        control.SetFloat("weak", weak);
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        //transform.Rotate(0, x, 0);
+        //transform.Translate(x, 0, z);
+
+        Debug.Log(jump);
+        Debug.Log(strong);
+        Debug.Log(weak);
+        Debug.Log(x);
+        Debug.Log(y);
+
+
+
+
     }
 }
